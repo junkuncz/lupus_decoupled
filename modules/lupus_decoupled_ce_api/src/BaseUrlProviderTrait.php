34889da30984b9b657a8aca98a792a6b04cd0e2b
ciprian.stavovei <ciprian.stavovei@3426791.no-reply.drupal.org>
2022-09-21 11:02:49 +0000

    Issue #3309813 by ciprian.stavovei, fago: View content on decoupled frontend

---
+++ modules/lupus_decoupled_ce_api/src/BaseUrlProviderTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\lupus_decoupled_ce_api;
+
+/**
+ * Allows setter injection and simple usage of the service.
+ */
+trait BaseUrlProviderTrait {
+
+  /**
+   * The base URL provider.
+   *
+   * @var \Drupal\lupus_decoupled_ce_api\BaseUrlProvider
+   */
+  protected $baseUrlProvider;
+
+  /**
+   * Sets the base URL provider.
+   *
+   * @param \Drupal\lupus_decoupled_ce_api\BaseUrlProvider $baseUrlProvider
+   *   The base URL provider.
+   *
+   * @return $this
+   */
+  public function setBaseUrlProvider(BaseUrlProvider $baseUrlProvider) {
+    $this->baseUrlProvider = $baseUrlProvider;
+    return $this;
+  }
+
+  /**
+   * Gets the base URL provider.
+   *
+   * @return \Drupal\lupus_decoupled_ce_api\BaseUrlProvider
+   *   The base URL provider.
+   */
+  public function getBaseUrlProvider() {
+    if (empty($this->baseUrlProvider)) {
+      $this->baseUrlProvider = \Drupal::service('lupus_decoupled_ce_api.base_url_provider');
+    }
+    return $this->baseUrlProvider;
+  }
+
+}
---
+++ modules/lupus_decoupled_ce_api/src/EventSubscriber/FrontendRedirectSubscriber.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\lupus_decoupled_ce_api\EventSubscriber;
+
+use drunomics\ServiceUtils\Core\Routing\CurrentRouteMatchTrait;
+use drunomics\ServiceUtils\Symfony\HttpFoundation\RequestStackTrait;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\lupus_decoupled_ce_api\BaseUrlProviderTrait;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Redirects routes to the decoupled frontend when access without /ce-api.
+ */
+class FrontendRedirectSubscriber implements EventSubscriberInterface {
+
+  use BaseUrlProviderTrait;
+  use CurrentRouteMatchTrait;
+  use RequestStackTrait;
+
+  /**
+   * An array of routes to redirect to the frontend.
+   *
+   * Contains 'entity.node.canonical' and possibly others.
+   *
+   * If a new route is added to frontend_routes,
+   * also add its path to lupus_decoupled_ce_api.frontend_paths.
+   * You can register a new frontend route by implementing
+   * a custom service provider.
+   *
+   * @var string[]
+   */
+  protected $frontendRoutes;
+
+  /**
+   * FrontendRedirectSubscriber constructor.
+   *
+   * @param string[] $frontendRoutes
+   *   The routes to redirect.
+   */
+  public function __construct(array $frontendRoutes) {
+    $this->frontendRoutes = $frontendRoutes;
+  }
+
+  /**
+   * Redirects entity.node.canonical requests to the frontend base_url.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The request event.
+   */
+  public function onKernelRequest(RequestEvent $event) {
+    if ($this->getCurrentRequest()->getRequestFormat() != 'html') {
+      // Do not redirect API responses.
+      return;
+    }
+
+    if ($this->getBaseUrlProvider()->getFrontendBaseUrl() == NULL) {
+      // Exit if frontend_base_url is not set.
+      return;
+    }
+
+    if (in_array($this->getCurrentRouteMatch()->getRouteName(), $this->frontendRoutes)) {
+      $route_match = $this->getCurrentRouteMatch();
+      // Handle canonical entity paths properly so any custom frontend paths
+      // apply correctly.
+      if (preg_match('/entity\.[a-z]+\.canonical/', $route_match->getRouteName())) {
+        $parameters = $route_match->getParameters()->all();
+        $entity = reset($parameters);
+        if ($entity && $entity instanceof EntityInterface) {
+          $redirect_url = $entity->toUrl('canonical', ['absolute' => TRUE])->toString();
+        }
+      }
+      if (!isset($redirect_url)) {
+        $redirect_url = $this->getBaseUrlProvider()->getFrontendBaseUrl() . $this->getCurrentRequest()->getRequestUri();
+      }
+      $event->setResponse(new TrustedRedirectResponse($redirect_url));
+      $event->stopPropagation();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest', 0],
+    ];
+  }
+
+}
