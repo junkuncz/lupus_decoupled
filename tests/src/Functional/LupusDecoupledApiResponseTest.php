7b01f782ce98799b562f5a7cec134a3d6bde968d
petarbasic <petar.basic@uns.ac.rs>
2022-09-23 12:50:16 +0200

    3311359: Add initial test

---
+++ tests/src/Functional/LupusDecoupledApiResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\Tests\lupus_decoupled\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test Lupus Decoupled features.
+ *
+ * @group lupus_decoupled
+ */
+class LupusDecoupledApiResponseTest extends BrowserTestBase {
+
+  /**
+   * The node to use for testing.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $node;
+
+  /**
+   * Path to created node.
+   *
+   * @var string
+   */
+  protected $nodePath;
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'node',
+    'lupus_decoupled',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Create Basic page node type.
+    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Basic page']);
+    $this->node = $this->drupalCreateNode();
+    $this->nodePath = 'ce-api/node/' . $this->node->id();
+  }
+
+  /**
+   * Tests if created node is accessible at api endpoint.
+   */
+  public function testExistingPageApiResponse() {
+    $this->drupalGet($this->nodePath);
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
